482a0b3297308cbedbf02aac5b025cbf774332de
potnox <potnox@gmail.com>
2014-01-29 13:39:34 +0100

    Complete chat
    Complete all exercise for chat in node

--- clase2/script.js
+++ clase2/script.js
@@ -1,10 +1,48 @@
-window.addEventListener("keydown",function(e){
-	if(e.keyCode === 13) sendMessage();
-},false);
 
-var port = promt("Enter port") || 4242;
+var port = prompt("Enter port") || 4242;
+var user = prompt("Enter Username") || "Loser";
+var input = document.getElementById('inputArea');
 var dir = "http://localhost:"+port;
+var board = document.getElementById("board");
+
+function newMessage(data)
+{
+	if(data !== undefined)
+	{
+		var user = data.username;
+		var msg = data.message;
+		var div = document.createElement('div');
+		div.className += "message";
+		var message = '<p><b>'+user+' </b>'+msg+'</p>';
+		div.innerHTML = message;
+		board.appendChild(div);
+	}
+}
+
+//Create socket
 var socket = io.connect(dir);
 socket.on('connect',function(){
-	console.log("Connected to server.");
+	newMessage({username:'Sistema',message:'Connected to chat!'});
+	socket.emit('newuser',user);
 });
+//If recieve new message, add this to board
+socket.on('newMessage',function(data){
+	newMessage(data);
+});
+
+
+var ENTER_KEY_CODE = 13;
+document.addEventListener('keydown', function (e) {
+	if (e.keyCode == ENTER_KEY_CODE && !e.shiftKey) sendMsg();
+});
+
+function sendMsg()
+{
+	//SEND MESSAGE TO SOCKET
+	var message = input.value.trim();
+	input.value = '';
+	if(message == "") return;
+	//if msg send to socket
+	var data = {username:user,message:message};
+	socket.emit('newMessage', data);
+}
--- clase2/socket.js
+++ clase2/socket.js
@@ -5,8 +5,24 @@ var logLevel = 1;
 //Modules
 var io = require('socket.io').listen(port);
 io.set("log level",logLevel);
+
 //Listen to socket
-io.sockets.on('connection',function(socket){
-	console.log("new Socket connected.");
+io.sockets.on('connection', function (client)
+{
+	client.on('newuser', function (username)
+	{
+		client.broadcast.emit('newMessage', {username:"Sistema", message: username + " has connected to the server!"});
+		client.set('username',username);
+	});
+	client.on('newMessage', function (data)
+	{
+		io.sockets.emit('newMessage',data);
+	})
+	client.on('disconnect',function ()
+	{
+		client.get('username',function (err,username) {
+			client.broadcast.emit('newMessage', {username:"Sistema", message: username + " has disconnected from the server!"});
+		});
+	});
 });
 console.log("Socket listen in port: "+port);
