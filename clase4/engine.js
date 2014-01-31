b7290a63852e0b064db9bb49f83a275f81130eb7
Javier <javix2003@hotmail.com>
2014-01-31 19:14:34 +0100

    Add requestAnimFrame into engine

--- clase4/engine.js
+++ clase4/engine.js
@@ -10,29 +10,39 @@ buffer.width = canvas.width;
 buffer.height = canvas.height;
 var ctxBuffer = buffer.getContext('2d');
 
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function()
+{
+  return window.requestAnimationFrame ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame ||
+         function(callback)
+         {
+           window.setTimeout(callback,1000/60);
+         };
+})();
+
 //GameObject Class
 function gameObject(obj)
 {
-	for(var prop in obj) this[prop] = obj[prop];
+  for(var prop in obj) this[prop] = obj[prop];
 }
 
 function genId()
 {
-	var date = +new Date();
-	return '#'+date+Math.floor(Math.random()*date);
+  var date = +new Date();
+  return '#'+date+Math.floor(Math.random()*date);
 }
 
 function objPrototype()
 {
-	var id = genId();
-	if(gameObjects[id] === undefined)
-	{
-		return new gameObject({'_id':id});
-	}
-	else
-	{
-		return newGameObject();
-	}
+  var id = genId();
+  if(gameObjects[id] === undefined)
+  {
+    return new gameObject({'_id':id});
+  }else{
+    return newGameObject();
+  }
 }
 
 //Game entities
@@ -45,10 +55,10 @@ var mouse_x,mouse_y;
 //Bind events
 //Keyboard events
 window.addEventListener('keydown',function (e){
-	keys[e.keyCode] = true;
+  keys[e.keyCode] = true;
 },false);
 window.addEventListener('keyup',function (e){
-	keys[e.keyCode] = false;
+  keys[e.keyCode] = false;
 },false);
 
 //Bind onresize
@@ -56,42 +66,42 @@ window.addEventListener('resize',resizeControl);
 
 //Bind mousemove
 window.addEventListener('mousemove',function (e){
-	var rect = canvas.getBoundingClientRect();
-	mouse_x = (e.clientX - rect.left);
-	mouse_y = (e.clientY - rect.top);
+  var rect = canvas.getBoundingClientRect();
+  mouse_x = (e.clientX - rect.left);
+  mouse_y = (e.clientY - rect.top);
 },false);
 
 //Mainloop support 
 function canvasUpdate()
 {
-	ctx.clearRect(0,0,canvas.width,canvas.height); //clean front canvas
-	ctx.drawImage(buffer,0,0); //copy buffer into front canvas
+  ctx.clearRect(0,0,canvas.width,canvas.height); //clean front canvas
+  ctx.drawImage(buffer,0,0); //copy buffer into front canvas
 }
 
 function resizeControl()
 {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	buffer.width = window.innerWidth;
-	buffer.height = window.innerHeight;
-	render(ctxBuffer);
-	canvasUpdate();
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  buffer.width = window.innerWidth;
+  buffer.height = window.innerHeight;
+  render(ctxBuffer);
+  canvasUpdate();
 }
 
 function logic(dt)
 {
-	for(var key in gameObjects)
-	{
-		if(gameObjects[key].logic) gameObjects[key].logic(dt);
-	}
+  for(var key in gameObjects)
+  {
+    if(gameObjects[key].logic) gameObjects[key].logic(dt);
+  }
 }
 
 function render(ctx)
 {
-	for(var key in gameObjects)
-	{
-		if(gameObjects[key].render) gameObjects[key].render(ctx);
-	}
+  for(var key in gameObjects)
+  {
+    if(gameObjects[key].render) gameObjects[key].render(ctx);
+  }
 }
 
 //main loop & deltatime
@@ -99,14 +109,14 @@ var oldDate = +new Date();
 var newDate,dt;
 function mainloop()
 {
-	requestAnimationFrame(mainloop);
-	newDate = +new Date();
-	dt = (newDate - oldDate)/1000;
-	oldDate = newDate;
-	ctxBuffer.clearRect(0,0,buffer.width,buffer.height);//clean buffer
-	logic(dt);
-	render(ctxBuffer); //draw all in buffer
-	canvasUpdate();
+  requestAnimFrame(mainloop);
+  newDate = +new Date();
+  dt = (newDate - oldDate)/1000;
+  oldDate = newDate;
+  ctxBuffer.clearRect(0,0,buffer.width,buffer.height);//clean buffer
+  logic(dt);
+  render(ctxBuffer); //draw all in buffer
+  canvasUpdate();
 }
 //Launch mainloop
-requestAnimationFrame(mainloop);
+requestAnimFrame(mainloop);
